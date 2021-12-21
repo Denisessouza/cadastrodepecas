e25f9603a00f9281744cc99855f1ef561df324d3
Denise Souza <95714393+Denisessouza@users.noreply.github.com>
2021-12-21 16:23:22 -0300

    Sistema de Cadastro de Peças Finalizado

---
+++ pecas.js
@@ -0,0 +1,21 @@
+
+let pesoPeca = 100
+if (pesoPeca > 100) {
+    console.log("Peso maior que 100 gramas, logo, podemos cadastrar a peça")
+
+} else { 
+    console.log ("Peso insuficiente, não é possível cadastrar a peça")
+}
+let NumeroPecas = 11
+if (NumeroPecas < 10) {
+    console.log ("Ainda há espaço na caixa, podemos cadastrar")
+} else {
+    console.log ("Não há espaço na caixa, não podemos cadastrar")
+}
+let nomePeca = "Disco"
+console.log ("o comprimento da peça é", nomePeca.length)
+if (nomePeca.length <3) {
+    console.log ("Nome de peça muito curto, não é possivel cadastrar")
+} else {
+    console.log ("Nome de peça adequado, podemos cadastrar")
+}
